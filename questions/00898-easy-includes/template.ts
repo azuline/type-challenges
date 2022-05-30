065d9de16ec1adaf70eb3ea2eb58e33527585f60
blissful <blissful@sunsetglow.net>
2022-05-30 03:25:27 -0400

    898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal } from '@type-challenges/utils'
+
+export type Includes<List extends readonly any[], Elem> =
+  List extends [infer Head, ...infer Tail]
+    ? Equal<Head, Elem> extends true
+      ? true
+      : Includes<Tail, Elem>
+    : false
